e8a405037a68b38c1cb34991503a197296367d0b
Lorenzo Beretta <43544206+guanaco96@users.noreply.github.com>
2018-10-13 17:20:42 +0200

    Update max_path_sum.cpp

--- lesson_4/max_path_sum.cpp
+++ lesson_4/max_path_sum.cpp
@@ -1,3 +1,7 @@
+/* This algorithm recursively performs a DFS backtracking
+* the lenght of furthest leave and keeping aside the value of
+* most distant leaves met so far*/
+
 /*Please note that it's Function problem i.e.
 you need to write your solution in the form of Function(s) only.
 Driver Code to call/invoke your function is mentioned above.*/
