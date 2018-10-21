12ec4d31dc2ab571f1c056836562a213e8908729
Lorenzo Beretta <43544206+guanaco96@users.noreply.github.com>
2018-10-21 23:15:56 +0200

    Update and rename leaders.cppsliding_window.cpp to sliding_window.cpp

---
+++ lesson_1/sliding_window.cpp
@@ -0,0 +1,29 @@
+/* This algorithm stores a decreasing sequence of values
+* that is sufficient to determine the max k-width windows 
+* array since we can ignore elements having value & index
+* smaller than the current added value.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int T;
+	cin>>T;
+	while(T-->0){
+		int n,k;
+		cin>>n>>k; 
+		deque<pair<int,int>> deq;
+		for(int i=0; i<n; i++){
+			if(!deq.empty() && i-deq.front().second>=k) deq.pop_front();
+			int x;
+			cin>>x;
+			while(!deq.empty() && deq.back().first<x) deq.pop_back();
+			deq.push_back(make_pair(x,i));
+			if(i>=k-1) cout<<deq.front().first<<" ";
+		}
+		cout<<endl;
+	}			
+	
+	return 0;
+}
