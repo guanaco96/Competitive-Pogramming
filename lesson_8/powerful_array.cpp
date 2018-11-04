8fbe3f23f2dce70efce0213ab95a9ff013d884ce
Lorenzo Beretta <43544206+guanaco96@users.noreply.github.com>
2018-11-04 23:44:09 +0100

    Update powerful_array.cpp

--- lesson_8/powerful_array.cpp
+++ lesson_8/powerful_array.cpp
@@ -6,41 +6,32 @@
  * Processing queries in this order gives us
  * the satisfying O(sqrt(n)*(n+t)) complexity.
  */
-
-#include<bits/stdc++.h>
+ #include<bits/stdc++.h>
 #define dbg(x) cout<<#x<<": "<<x<<endl;
 using namespace std;
 typedef long long ll;
-constexpr int N=100001;
+constexpr ll N=200001;
+constexpr ll M=1000001;
 
-int color[N];
-vector<vector<int>> tree;
-vector<pair<int,int>> vert_to_sub;
+ll a[N];
+ll val[M];
+map<pair<ll,ll>,ll> q_to_res;
 
-void DFS(int index, vector<int> lin){
-	vert_to_sub[index].first=lin.size();
-	lin.push_back(index);
-	for(vector<int>::iterator it=tree[index].begin(); it!=tree[index].end(); it++){
-		DFS(*it, lin);
-	}
-	vert_to_sub[index].second=lin.size()-1;
-}
-
-ll trans(int os, int oe, int ns, int ne){
+ll trans(ll os, ll oe, ll ns, ll ne){
 	ll tot=0;
 	if(os>ns){
-		for(int i=ns; i<os; i++){
+		for(ll i=ns; i<os; i++){
 			tot+=a[i]*(2*val[a[i]]+1);
 			val[a[i]]++;
 		}
 	}
 	else{
-		for(int i=os; i<ns; i++){
+		for(ll i=os; i<ns; i++){
 			val[a[i]]--;
 			tot-=a[i]*(2*val[a[i]]+1);			
 		}
 	}
-	for(int i=oe; i<ne; i++){
+	for(ll i=oe+1; i<=ne; i++){
 		tot+=a[i]*(2*val[a[i]]+1);
 		val[a[i]]++;
 	}
@@ -51,21 +42,39 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	int n, m;
-	cin>>n>>m;
-	for(int i=0; i<n; i++) cin>>color[i];
-	tree=vector<vector<int>>(n+1);
-	for(int i=0; i<n-1; i++){
-		int a, b;
-		cin>>a>>b;
-		tree[a].push_back(b);
-		tree[b].push_back(a);
+	ll n, t;
+	cin>>n>>t;
+	for(ll i=1; i<n+1; i++) cin>>a[i];
+	ll sq=sqrt(n);
+	ll bl=n/sq+1;
+	vector<vector<pair<ll,ll>>> query(bl);
+	// to remember queries numbers
+	vector<pair<ll,ll>> num_to_q(t); 
+	for(ll i=0; i<t; i++){
+		pair<ll,ll> p;
+		//ACHTUNG: in order to use default lexicographical order on
+		// pairs we'r saving pairs like (r,l).
+		cin>>p.second>>p.first;
+		query[p.second/sq].push_back(p);
+		num_to_q[i]=p;
 	}
-	vert_to_sub=vector<pair<int,int>>(n);
-	vector<int> lin;
-	DFS(1, lin);
-	
-	
+	for(ll i=0; i<bl; i++){
+		vector<pair<ll,ll>>::iterator it=query[i].begin();
+		if(it==query[i].end()) continue;
+		sort(query[i].begin(), query[i].end());
+		for(ll i=0; i<M; i++) val[i]=0;
+		ll tot=0;
+		for(ll i=it->second; i<=it->first; i++){
+			tot+=a[i]*(2*val[a[i]]+1);
+			val[a[i]]++;
+		}
+		q_to_res.insert(make_pair(*it,tot));
+		for(it++; it!=query[i].end(); it++){
+			tot+=trans((it-1)->second, (it-1)->first, it->second, it->first);
+			q_to_res.insert(make_pair(*it,tot));
+		}
+	}
+	for(ll i=0; i<t; i++) cout<<q_to_res[num_to_q[i]]<<endl;	
 	
 	return 0;
 }
